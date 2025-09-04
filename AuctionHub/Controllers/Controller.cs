072629bdbdcb37dbca6026677f00b679ccaccc0f
jinxinzero <jinxinzero7@gmail.com>
2025-09-04 11:30:55 +0300

    Init proj

---
+++ AuctionHub/Controllers/Controller.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionHub.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Controller : ControllerBase
+    {
+        
+    }
+}
