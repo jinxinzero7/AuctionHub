aa65ac6449347b6e34f70c14cafd308280528525
niko <jinxinzero7@gmail.com>
2025-10-14 21:37:04 +0300

    refactor(architecture): complete service layer restructuring with proper namespaces and DI

--- Application/Services/CurrentUserService.cs
+++ Application/Services/CurrentUserService.cs
@@ -7,7 +7,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Infrastructure.Services
+namespace Application.Services
 {
     public class CurrentUserService : ICurrentUserService
     {
